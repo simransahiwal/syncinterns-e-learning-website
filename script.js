3338418f20cf2e838c7b29c7f975a15ca09bdd3c
simransahiwal <134322061+simransahiwal@users.noreply.github.com>
2023-07-29 22:50:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+function home1()
+{
+
+   homes.style.display = 'block';
+   courses.style.display='none';
+   tests.style.display = 'none';
+   about.style.display='none';
+   contact.style.display='none';
+   signin.style.display = 'none';
+}
+function courses1()
+{
+   homes.style.display = 'none';
+   courses.style.display='block';
+   tests.style.display = 'none';
+   about.style.display='none';
+   contact.style.display='none';
+   signin.style.display = 'none';
+}
+function test1()
+{
+   homes.style.display = 'none';
+   courses.style.display='none';
+   tests.style.display = 'block';
+   about.style.display='none';
+   contact.style.display='none';
+   signin.style.display = 'none';
+}
+function about1()
+{
+   homes.style.display = 'none';
+   courses.style.display='none';
+   tests.style.display = 'none';
+   about.style.display='block';
+   contact.style.display='none';
+   signin.style.display = 'none';
+}
+function contact1()
+{
+   homes.style.display = 'none';
+   courses.style.display='none';
+   tests.style.display = 'none';
+   about.style.display='none';
+   contact.style.display='block';
+   signin.style.display = 'none';
+}
+function login1()
+{
+   homes.style.display = 'none';
+   courses.style.display='none';
+   tests.style.display = 'none';
+   about.style.display='none';
+   contact.style.display='none';
+   signin.style.display = 'block';
+}
